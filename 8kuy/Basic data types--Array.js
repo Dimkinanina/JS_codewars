cced05485005b3e506b0b71ba73d8ce4fdab7f89
NinaB <dimkinanina@gmail.com>
2020-03-24 22:11:43 -0400

    JavaScript solutions

---
+++ 8kuy/Basic data types--Array.js
@@ -0,0 +1,25 @@
+/*
+Task
+I've written five function, each function receives a parameter: arr(an array),
+you should coding something with arr.
+ */
+
+
+function getLength(arr){
+    return arr.length;
+}
+function getFirst(arr){
+    return arr[0];
+}
+function getLast(arr){
+    return arr[arr.length - 1];
+}
+function pushElement(arr){
+    var el=1;
+    arr.push(el)
+    return arr;
+}
+function popElement(arr){
+    arr.pop();
+    return arr;
+}
